870cfae61944643f8e27122da86ab4d5caab046e
podegal <emmaomene@yahoo.com>
2023-05-25 19:31:41 -0700

    bytecode

--- bc1.m
+++ bc1.m
@@ -1,15 +1,16 @@
-push 0 Push 0 onto the stack$
-push 1 Push 1 onto the stack$
-$
-push 2$
-  push 3$
-                   pall    $
-$
-$
-                           $
-push 4$
-$
-    push 5    $
-      push    6        $
-$
-pall This is the end of our program. Monty is awesome!$
+queue
+push 1
+push 2
+push 3
+pall
+stack
+push 4
+push 5
+push 6
+pall
+add
+pall
+queue
+push 11111
+add
+pall
